8b3c9796ce7087503b2cae28b27a8642fea837cd
26-Anuj <59211195+Amitk6228@users.noreply.github.com>
2020-11-03 02:20:13 +0530

    editing

--- js/owlSlider.js
+++ js/owlSlider.js
@@ -55,3 +55,30 @@ jQuery("#carousel").owlCarousel({
         }
     }
   });
+  jQuery("#carousel3").owlCarousel({
+    autoplay: true,
+    lazyLoad: true,
+    loop: true,
+    margin: 20,
+     /*
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 1500,
+    smartSpeed:1300,
+    center:true,
+    nav: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+  });
